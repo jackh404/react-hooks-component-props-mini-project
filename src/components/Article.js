e193ce196b843b3e179b94123ba68449243d7fe4
Jack Henza <jackhenza@gmail.com>
2023-10-25 15:31:34 -0400

    does the emoji time to read

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,10 +1,25 @@
 import React from "react";
 
-function Article({title,date = "January 1, 1970",preview}){
+function Article({title,date = "January 1, 1970",preview,minutes}){
+    let timeMoji
+    let timeMojis = ''
+    let cups
+    console.log(minutes)
+    if(minutes < 30){
+        cups = Math.floor(minutes/5)
+        timeMoji = '☕'
+    } else {
+        cups = Math.floor(minutes/10)
+        timeMoji = '🍱'
+    }
+    console.log(cups)
+    for(let i=0; i<cups; i++){
+        timeMojis += timeMoji
+    }
     return (
         <article>
             <h3>{title}</h3>
-            <small>{date}</small>
+            <small>{date} • {timeMojis} {minutes} minute read</small>
             <p>{preview}</p>
         </article>
     )
--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -2,7 +2,7 @@ import React from "react";
 import Article from "./Article";
 
 function ArticleList({posts}){
-    const articles = posts.map(post => <Article key = {post.id} title = {post.title} date = {post.date} preview = {post.preview}/>)
+    const articles = posts.map(post => <Article key = {post.id} title = {post.title} date = {post.date} preview = {post.preview} minutes = {post.minutes}/>)
     return (
         <main>
             {articles}
